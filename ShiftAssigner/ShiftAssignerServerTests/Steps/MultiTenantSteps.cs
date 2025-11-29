5d2a0dc9ccdabc6f7d7f7c57cf625327b2ad58ef
natanelherkof <natanelherkof@gmail.com>
2025-11-29 10:09:31 +0200

    Self code review

--- ShiftAssigner/ShiftAssignerServerTests/Steps/MultiTenantSteps.cs
+++ ShiftAssigner/ShiftAssignerServerTests/Steps/MultiTenantSteps.cs
@@ -1,38 +1,13 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 using Reqnroll;
-using ShiftAssignerServer.Controllers;
-using ShiftAssignerServer.Models.Stuff;
 using ShiftAssignerServer.Requests;
 using ShiftAssignerServer.Tests.Common;
-using ShiftAssignerServer.Tests.Infrastructure;
-using Xunit;
 
 namespace ShiftAssignerServer.Tests.Steps;
 
 [Binding]
 public class MultiTenantSteps : TwoTenantsStep
 {
-    private const string TenantA_Context = "TenantA_Data";
-    private const string TenantB_Context = "TenantB_Data";
-    private const string TenantA_Response_Context = "TenantA_Response";
-    private const string TenantB_Response_Context = "TenantB_Response";
-    private const string TenantA_Leaders_Context = "TenantA_Leaders";
-    private const string TenantB_Leaders_Context = "TenantB_Leaders";
-    private const string TenantA_Workers_Context = "TenantA_Workers";
-    private const string TenantB_Workers_Context = "TenantB_Workers";
-    
-    // Additional context keys for worker registration validation
-    private const string WorkerRegistrationResponse_Context = "WorkerRegistrationResponse";
-    private const string RegistrationException_Context = "RegistrationException";
-    private const string HttpResponse_Context = "HttpResponse";
-    private const string ErrorResponseBody_Context = "ErrorResponseBody";
-
     public MultiTenantSteps(ScenarioContext scenarioContext) : base(scenarioContext)
     {
     }
